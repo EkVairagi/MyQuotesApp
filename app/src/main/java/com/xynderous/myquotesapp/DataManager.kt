a986c5d3e3c69546600e5a9a3f4caf613a1861cd
sannayak4 <Sngm@8527>
2023-11-10 02:07:39 +0530

    Initial commit

---
+++ app/src/main/java/com/xynderous/myquotesapp/DataManager.kt
@@ -0,0 +1,41 @@
+package com.xynderous.myquotesapp
+
+import android.content.Context
+import androidx.compose.runtime.mutableStateOf
+import com.google.gson.Gson
+import com.xynderous.myquotesapp.models.Quote
+
+object DataManager {
+
+    var data = emptyArray<Quote>()
+    var isDataLoaded = mutableStateOf(false)
+    var currentPage = mutableStateOf(Pages.LISTING)
+
+    var currentQuote: Quote? = null
+
+    fun loadAssetsFromFile(context: Context) {
+
+        val inputStream = context.assets.open("quote.json")
+        val size = inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+        val json = String(buffer, Charsets.UTF_8)
+
+        val gson = Gson()
+        data = gson.fromJson(json, Array<Quote>::class.java)
+
+        isDataLoaded.value = true
+
+    }
+
+    fun switchPages(quote: Quote?) {
+        if (currentPage.value == Pages.LISTING) {
+            currentQuote = quote
+            currentPage.value = Pages.DETAIL
+        } else {
+            currentPage.value = Pages.LISTING
+        }
+    }
+
+}
---
+++ app/src/main/java/com/xynderous/myquotesapp/screens/QuoteList.kt
@@ -0,0 +1,17 @@
+package com.xynderous.myquotesapp.screens
+
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import com.xynderous.myquotesapp.models.Quote
+
+@Composable
+fun QuoteList(data: Array<Quote>, onClick: (quote: Quote)-> Unit){
+
+    LazyColumn(content = {
+        items(data){
+            QuoteListItem(quote = it, onClick)
+        }
+    })
+    
+}
